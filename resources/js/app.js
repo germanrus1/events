3f73d3ff92774e14ec4a54f22d71e39594858a1d
German <shuriksful@gmail.com>
2022-04-22 20:15:20 +0400

    Main page layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,11 @@
 require('./bootstrap');
-
 require('alpinejs');
+require('bootstrap');
 
 import { createApp } from 'vue';
-import router from './router'
+import router from './router';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventsIndex from './components/events/EventsIndex.vue';
 
